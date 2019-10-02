7088feb0fc2835e0a440925065e9f6082d734602
Shuning.D <dong.472@osu.edu>
2019-10-01 20:47:27 -0400

    first activity and fragment method for homescreen.
    Signed-off-by: Shuning.D <dong.472@osu.edu>

--- app/src/main/java/com/example/collegelife/ui/main/MainActivity.java
+++ app/src/main/java/com/example/collegelife/ui/main/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.collegelife.ui.main;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.collegelife.R;
 
@@ -13,7 +16,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        menufragment mainMenu = new menufragment();
-        getSupportFragmentManager().beginTransaction().add(R.id.container, mainMenu).commit();
+        //menufragment mainMenu = new menufragment();
+        //getSupportFragmentManager().beginTransaction().add(R.id.container, mainMenu).commit();
+
+
+        Button start_button = (Button) findViewById(R.id.startbutton);
+
+        start_button.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                //move to player page after click the start button
+                Intent startIntent = new Intent(getApplicationContext(), player1page.class);
+                startActivity(startIntent);
+            }
+        });
+
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+
+
 }
---
+++ app/src/main/java/com/example/collegelife/ui/main/player1page.java
@@ -0,0 +1,16 @@
+package com.example.collegelife.ui.main;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.collegelife.R;
+
+public class player1page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player1page);
+    }
+}
