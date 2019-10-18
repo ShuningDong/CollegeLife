2607a3acd5796c0a24f1da71d645c1b4cb9f9955
nunderwood1102 <nunderwood64@ymail.com>
2019-10-17 21:49:13 -0400

    added character fragment and character class

---
+++ app/src/main/java/com/example/collegelife/ui/main/Character.java
@@ -0,0 +1,43 @@
+package com.example.collegelife.ui.main;
+
+public class Character {
+
+    private String Name;
+    private int GPA;
+    private double Debt;
+    private String Major;
+
+    // constructor
+    public Character(String name) {
+        Name = name;
+        GPA = 4;
+        Debt = 0;
+        Major = "";
+    }
+
+
+    ///////// methods /////////
+    public String getName(){
+        return this.Name;
+    }
+
+    public int getGPA(){
+        return this.GPA;
+    }
+
+    public double getDebt(){
+        return this.Debt;
+    }
+
+    public void addLoan(double amount){
+        this.Debt += amount;
+    }
+
+    public void subtractDebt(double amount){
+        this.Debt -= amount;
+    }
+
+    public String getMajor(){
+        return this.Major;
+    }
+}
---
+++ app/src/main/java/com/example/collegelife/ui/main/CharacterActivity.java
@@ -0,0 +1,61 @@
+package com.example.collegelife.ui.main;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.collegelife.R;
+
+public class CharacterActivity extends AppCompatActivity {
+    private static final String TAG = "Character_Activity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_character);
+
+        CharacterFragment charMenu = new CharacterFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.container, charMenu).commit();
+
+        Log.d(TAG, "onCreate is called for character activity");
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart is called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume is called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause is called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop is called");
+    }
+
+    @Override
+    // Shoulde be called before onPause
+    protected void onSaveInstanceState(Bundle outstate){
+        super.onSaveInstanceState(outstate);
+    }
+
+    @Override
+    // Should be called after onStart and onResume
+    protected void onRestoreInstanceState(Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+}
