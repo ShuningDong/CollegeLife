5f9d65a5bb195dae39b59833819a3f418c5f2ca8
Dorothy <dordor0709@gmail.com>
2019-10-27 23:29:46 -0400

    Add grid

---
+++ app/src/main/java/com/example/collegelife/ui/main/BoardActivity.java
@@ -0,0 +1,15 @@
+package com.example.collegelife.ui.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
+import android.os.Bundle;
+import com.example.collegelife.R;
+
+public class BoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board);
+    }
+}
