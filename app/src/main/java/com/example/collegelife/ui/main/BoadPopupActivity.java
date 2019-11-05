b4bae350354e3d1cc6a32587dff63942e359be0a
Dorothy <dordor0709@gmail.com>
2019-11-04 21:45:13 -0500

    Grid popup UI

---
+++ app/src/main/java/com/example/collegelife/ui/main/BoadPopupActivity.java
@@ -0,0 +1,41 @@
+package com.example.collegelife.ui.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.example.collegelife.R;
+
+public class BoadPopupActivity extends AppCompatActivity {
+    private static final String TAG = "popup_board_Activity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boad_popup);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause is called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop is called");
+    }
+
+    @Override
+    // Shoulde be called before onPause
+    protected void onSaveInstanceState(Bundle outstate){
+        super.onSaveInstanceState(outstate);
+    }
+
+    @Override
+    // Should be called after onStart and onResume
+    protected void onRestoreInstanceState(Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+}
